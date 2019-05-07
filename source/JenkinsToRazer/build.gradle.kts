1cc81954b7bce927f469776aa1ad817836c93394
Thomas Wimmer <tom.wimmer@hotmail.com>
2019-05-07 18:12:56 +0200

    Created targets for linux, windows and shared code.

--- source/JenkinsToRazer/build.gradle.kts
+++ source/JenkinsToRazer/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("multiplatform") version "1.3.21"
+    kotlin("multiplatform") version "1.3.31"
 }
 
 repositories {
@@ -7,14 +7,26 @@ repositories {
 }
 
 kotlin {
-  linuxX64("native") {
-    binaries {
-      executable()
+    linuxX64("linux") {
+        binaries {
+            executable()
+        }
     }
-  }
-}
 
-tasks.withType<Wrapper> {
-  gradleVersion = "5.3.1"
-  distributionType = Wrapper.DistributionType.ALL
+    mingwX64("windows") {
+        binaries {
+            executable()
+        }
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        linuxX64("linux").compilations["main"].defaultSourceSet  { /* ... */ }
+        mingwX64("windows").compilations["main"].defaultSourceSet  { /* ... */ }
+    }
 }
