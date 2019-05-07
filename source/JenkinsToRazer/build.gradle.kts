a65f961704bdf02400cdd573c458a9db626c0ecf
Thomas Wimmer <tom.wimmer@hotmail.com>
2019-05-07 15:55:13 +0200

    Initial commit. Based on Kotlin Native Hello World.

---
+++ source/JenkinsToRazer/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("multiplatform") version "1.3.21"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+  linuxX64("native") {
+    binaries {
+      executable()
+    }
+  }
+}
+
+tasks.withType<Wrapper> {
+  gradleVersion = "5.3.1"
+  distributionType = Wrapper.DistributionType.ALL
+}
