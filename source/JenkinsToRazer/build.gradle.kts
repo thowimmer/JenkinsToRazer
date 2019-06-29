5eb097ab4d77d9cec4e3c06f5e153d90804754a8
Thomas Wimmer <Thomas.Wimmer@netcetera.com>
2019-06-29 12:22:57 +0200

    Update of build tools and dependencies: * Kotlin 1.3.40 * Ktor 1.2.2 * kotlinx.serialization 0.11.1 * kotlinx.coroutines 1.3.0-M2

--- source/JenkinsToRazer/build.gradle.kts
+++ source/JenkinsToRazer/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.apache.tools.ant.taskdefs.condition.Os
 
-val ktorVersion = "1.1.5"
-val kotlinxSerializationVersion = "0.11.0"
-val kotlinxCoroutinesVersion = "1.3.0-M1"
+val ktorVersion = "1.2.2"
+val kotlinxSerializationVersion = "0.11.1"
+val kotlinxCoroutinesVersion = "1.3.0-M2"
 
 plugins {
-    id("kotlin-multiplatform") version "1.3.31"
-    id("kotlinx-serialization") version "1.3.31"
+    id("kotlin-multiplatform") version "1.3.40"
+    id("kotlinx-serialization") version "1.3.40"
 }
 
 repositories {
@@ -35,7 +35,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib-common"))
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
-                implementation("io.ktor:ktor-client-json:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$kotlinxSerializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:$kotlinxCoroutinesVersion")
             }
@@ -45,7 +45,8 @@ kotlin {
             dependsOn(commonMain)
             dependencies {
                 implementation("io.ktor:ktor-client-curl:$ktorVersion")
-                implementation("io.ktor:ktor-client-json-native:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization-linuxx64:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-linuxx64:$kotlinxSerializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-linuxx64:$kotlinxCoroutinesVersion")
             }
         }
@@ -54,8 +55,8 @@ kotlin {
             dependsOn(commonMain)
             dependencies {
                 implementation("io.ktor:ktor-client-curl:$ktorVersion")
-                implementation("io.ktor:ktor-client-json-native:$ktorVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-native:$kotlinxSerializationVersion")
+                implementation("io.ktor:ktor-client-serialization-mingwx64:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-mingwx64:$kotlinxSerializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-windowsx64:$kotlinxCoroutinesVersion")
             }
         }
